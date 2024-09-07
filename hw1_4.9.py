45fd457447392f8de43e09029069e0712a4c37c7
misheldror <misheldror@gmail.com>
2024-09-07 23:33:25 +0300

    Add files via upload

---
+++ hw1_4.9.py
@@ -0,0 +1,36 @@
+# START
+
+id: str = input("Enter your ID: ")
+fname: str = input("Enter your first name: ")
+lname: str = input("Enter your last name: ")
+h: float = input("Enter your height: ")
+yob: int = input("Enter year of birth: ")
+print(f"id: {id} name: {lname}, {fname} height: {h} year of birth: {yob}")
+
+# END
+
+# START
+
+id1: str = input("Enter your ID: ")
+fname1: str = input("Enter your first name: ")
+lname1: str = input("Enter your last name: ")
+h1: float = input("Enter your height: ")
+yob1: int = input("Enter year of birth: ")
+
+id2: str = input("Enter your ID: ")
+fname2: str = input("Enter your first name: ")
+lname2: str = input("Enter your last name: ")
+h2: float = input("Enter your height: ")
+yob2: int = input("Enter year of birth: ")
+
+id3: str = input("Enter your ID: ")
+fname3: str = input("Enter your first name: ")
+lname3: str = input("Enter your last name: ")
+h3: float = input("Enter your height: ")
+yob3: int = input("Enter year of birth: ")
+
+print(f"id: {id1:^7} name: {lname1:^7} {fname1:^7} height: {h1:^7} year of birth: {yob1:^7}")
+print(f"id: {id2:^7} name: {lname2:^7} {fname2:^7} height: {h2:^7} year of birth: {yob2:^7}")
+print(f"id: {id3:^7} name: {lname3:^7} {fname3:^7} height: {h3:^7} year of birth: {yob3:^7}")
+
+# END
